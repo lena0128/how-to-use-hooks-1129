808368eadf6a75e4c95c435d6a429913a5d4b170
Lena Hou <lenahou2013@gmail.com>
2021-11-30 11:52:34 -0500

    updates

--- src/FunctionalApp.js
+++ src/FunctionalApp.js
@@ -1,10 +1,31 @@
 import React, {useState, useEffect} from 'react'
 
 const FunctionalApp = (props) => {
+
+    const [count, setCount] = useState(0)
+    // we are assigning `count` to 0
+    // we are assigning `setCount` as a function that is going to update the state
+
+    const myFunc = () => {
+        console.log("inside myFunc")
+    }
+    
+    // 1. if we ONLY pass in a callback function as an argument to useEffect(), 
+    // this callback function will behave as componentDidMount, componentDidUpdate, or componentWillUnmount
+    // which means this callback function will be invoked when compnenent mounted, updated, or unmounted. 
+    useEffect(myFunc)
+
+
+    // 2. if we pass a second argument - everytime we update the count state, the callback function is being invoked
+    // we are basically saying: invoke the callback function if the count changes
+    useEffect(myFunc, [count])
+
+    
     return(
         <div style={{textAlign: "center", marginTop: "40px", fonSize: "45px"}} >
-            <button style={{fontSize: "20px", color: "#FBBF29"}} onClick={() => console.log("clicked")} > - </button>
-            <button style={{fontSize: "20px", color: "#FBBF29"}} onClick={() => console.log("clicked")} > + </button>
+            {count} <br />
+            <button style={{fontSize: "20px", color: "#FBBF29"}} onClick={() => setCount(count - 1)} > - </button>
+            <button style={{fontSize: "20px", color: "#FBBF29"}} onClick={() => setCount(count + 1)} > + </button>
         </div>
     )
 }
